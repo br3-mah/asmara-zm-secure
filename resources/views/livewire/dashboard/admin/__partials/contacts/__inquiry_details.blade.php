6fec8160de9cbc88b96a14c71a6ff8cd27c29585
br3-mah <nyeleti.bremah@gmail.com>
2023-04-12 17:37:05 +0200

    Fixing Booktrait last

--- resources/views/livewire/dashboard/admin/__partials/contacts/__inquiry_details.blade.php
+++ resources/views/livewire/dashboard/admin/__partials/contacts/__inquiry_details.blade.php
@@ -1,18 +1,18 @@
-<div wire:ignore.self class="modal fade" id="inquiryDetailModalLong">
+<div class="modal fade" id="inquiryDetailModalLong">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header bg-primary">
-                <h5 class="modal-title text-white">SUBJECT: {{ $contact_info->subject ?? ''}}</h5>
+                <h5 class="modal-title text-white">SUBJECT: {{ $contact_info !== null ?  $contact_info->subject : ''}}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal">
                 </button>
             </div>
             <div class="modal-body">
-                {{ $contact_info->message ?? '' }}
+                {{ $contact_info !== null ? $contact_info->message : ''  }}
                 <br/>
-                <p>{{ $contact_info->phone ?? '' }}</p>
-                <p>{{ $contact_info->email ?? '' }}</p>
+                <p>{{ $contact_info !== null ?  $contact_info->phone : '' }}</p>
+                <p>{{ $contact_info !== null ?  $contact_info->email : '' }}</p>
                 <br/>
-                {{ $contact_info->created_at->diffForHumans() }}
+                {{ $contact_info !== null ?  $contact_info->created_at->diffForHumans() : '' }}
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
